2f9e963fb848f5967436e316eb0546031eba2c1c
Paul WB <higgsbison@gmail.com>
2016-12-28 11:16:24 -0800

    create booklist container

---
+++ src/reducers/books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'The Two Towers'},
+    {title: 'Crime and Punishments'},
+    {title: 'Being and Time'},
+    {title: 'Being and Nothingness'},
+  ]
+}
